d48f2e9e7c40946e8a5bab4746ee8cb460873d3c
Odagawa <xtctl.1234@gmail.com>
2019-09-12 22:04:44 +0900

    Finish mat basic

--- src/main/scala/matrix/Mtrx2.scala
+++ src/main/scala/matrix/Mtrx2.scala
@@ -15,6 +15,8 @@ class Mtrx2[T](val rows: Int, val cols: Int, val data: Array[T]) {
   }
 
   def apply(i: Int, j: Int): T = {
+    if (j >= cols)
+      throw new IndexOutOfBoundsException(s"j must be less than cols: $cols")
     data(i * cols + j)
   }
 
@@ -26,9 +28,9 @@ class Mtrx2[T](val rows: Int, val cols: Int, val data: Array[T]) {
 
   override def toString: String = {
     var s = "[\n"
-    for (k <- 0 to rows*cols; v <- data) {
-      if (k % cols == 0) s += " ["
-      s += v.toString + " "
+    for (k <- 0 until rows*cols) {
+      if (k % cols == 0) s += " [ "
+      s += data(k).toString + " "
       if (k % cols == cols-1) s += "]\n"
     }
     s + "]"
@@ -43,9 +45,13 @@ class ValueMtrx2[T <: AnyVal](override val rows: Int, override val cols: Int, ov
       throw new IndexOutOfBoundsException(s"Shape is not same: ${(rows, cols)} and ${(otherMat.rows, otherMat.cols)}")
     }
     val arr = Array.ofDim[S](cols*rows)
-    for (k <- 0 to rows*cols) {
+    for (k <- 0 until rows*cols) {
       arr(k) = f(data(k), otherMat.data(k))
     }
     new ValueMtrx2[S](rows, cols, arr)
   }
+
+//  def +[U >: T <: AnyVal](otherMat: ValueMtrx2[U]) = {
+//    calcEach(otherMat, (x, y): (T, T) => x + y )
+//  }
 }
--- src/test/scala/matrix/Mtrx2Test.scala
+++ src/test/scala/matrix/Mtrx2Test.scala
@@ -3,16 +3,16 @@ import org.scalatest._
 
 class Mtrx2Test extends FlatSpec with DiagrammedAssertions{
 
-  val mat = new Mtrx2(3,
+  val mat = new Mtrx2(2,
     Array(1,2,3,
           4,5,6))
-  val matSame = new Mtrx2(3,2,
+  val matSame = new Mtrx2(2,3,
     Array(1,2,3,
           4,5,6))
 
   "rows, cols" should "be the number of rows, cols" in {
-    assert(mat.rows == 3)
-    assert(mat.cols == 2)
+    assert(mat.rows == 2)
+    assert(mat.cols == 3)
   }
 
   "same matrix" should "be recognized as same" in {
@@ -26,5 +26,22 @@ class Mtrx2Test extends FlatSpec with DiagrammedAssertions{
       new Mtrx2(2, 3, Array(1,2,3,4))
     )
   }
+
+  "access" should "be proper" in {
+    assert(mat(0, 0) === 1)
+    assert(mat(1, 2) === 6)
+    intercept[IndexOutOfBoundsException](
+      mat(0, 3)
+    )
+    intercept[IndexOutOfBoundsException](
+      mat(3, 0)
+    )
+  }
+
+//  val vmat = new ValueMtrx2[Int](2,
+//    Array(1,2,3,
+//          4,5,6))
+
+
 
 }
