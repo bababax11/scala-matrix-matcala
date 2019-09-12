40dc5f3eb66666730d831b54a1c9db18c8b83ab9
Odagawa <xtctl.1234@gmail.com>
2019-09-12 14:58:45 +0900

    Define value mat

--- src/main/scala/matrix/Mtrx2.scala
+++ src/main/scala/matrix/Mtrx2.scala
@@ -1,5 +1,7 @@
 package bababax11.matcala.matrix
 
+import scala.collection.mutable.ArrayBuffer
+
 class Mtrx2[T](val rows: Int, val cols: Int, val data: Vector[Vector[T]]) {
 
   def this(data: Vector[Vector[T]]) = {
@@ -17,13 +19,33 @@ class Mtrx2[T](val rows: Int, val cols: Int, val data: Vector[Vector[T]]) {
 
   override def toString: String = {
     var s = "[\n"
-    for(array <- data) {
+    for(vec <- data) {
       s +=" [ "
-      for(num <- array) {
+      for(num <- vec) {
         s += num.toString + " "
       }
       s += "]\n"
     }
     s + "]"
+  }
+}
+
+class ValueMtrx2[T](override val rows: Int, override val cols: Int, override val data: Vector[Vector[T]])
+  extends Mtrx2[T](rows, cols, data) {
+
+  def calcEach(mat: ValueMtrx2[T], f: (T,T) -> S): ValueMtrx2[S] = {
+    if(rows != mat.rows || cols != mat.cols) {
+      throw new IndexOutOfBoundsException("Shape is not same")
+    }
+    val arr = Array.ofDim(rows)
+    for(i <- 0 to rows; vec <- data; otherVec <-mat.data) {
+      val ar = Array.ofDim(cols)
+      for(j <- 0 to cols; v <- vec; ov <- otherVec) {
+          ar(j) = f(v, ov)
+      }
+      arr(i) = ar.toVector
+    }
+    val vec = arr.toVector
+    new ValueMtrx2[Any](rows, cols, vec)
   }
 }
